9ecc29d7a9cd929de558caf04e6dbba28e7c4ab0
deepak-sreekumar <deepakstark@gmail.com>
2022-08-25 07:54:44 +0530

    add json logger

--- src/safe_security_logger/__init__.py
+++ src/safe_security_logger/__init__.py
@@ -1,4 +1,7 @@
 import sys
+from safe_security_logger.logger import (
+    getLogger
+)
 
 if sys.version_info[:2] >= (3, 8):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
@@ -10,6 +13,7 @@ try:
     # Change here if project is renamed and does not equal the package name
     dist_name = "safe-security-logger"
     __version__ = version(dist_name)
+    __all__ = [ "getLogger" ]
 except PackageNotFoundError:  # pragma: no cover
     __version__ = "unknown"
 finally:
---
+++ src/safe_security_logger/logger.py
@@ -0,0 +1,25 @@
+import argparse
+import logging
+import sys
+
+__author__ = "deepak.s@sasfe.security"
+__copyright__ = "deepak.s@safe.security"
+__license__ = "MIT"
+
+import os.path
+import logging
+import traceback
+from pythonjsonlogger import jsonlogger
+
+def getLogger(name):
+    logger = logging.getLogger(name)
+
+    # Logs will be written to console
+    console_handler = logging.StreamHandler(sys.stdout)
+    formatter = jsonlogger.JsonFormatter('%(asctime)s %(name)s %(levelname)s  %(message)s  %(module)s %(funcName)s ')
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+    # default level would be INFO
+    logger.setLevel(logging.INFO)
+    return logger
